1719cfcce9c740234b4f101e1530372d7ca57695
Carl Parrish <carl.parrish@gmail.com>
2020-07-05 22:59:09 +0000

    --wip-- [skip ci]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import { displayPartsToString } from 'typescript';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-calculator';
+  title = 'Calculator';
+
+  calcForm = new FormGroup({});
+  options: FormlyFormOptions = {};
+  model = { display: '' };
+
+  numericKeys = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+  };
+
+  fields: FormlyFieldConfig[] = [{
+    key: 'display',
+    type: 'input',
+    templateOptions: {
+      id: 'display',
+      value: 'my display'
+     }
+  }];
+
+  constructor() {
+
+    /* for (const [key, value] of Object.entries(this.numericKeys)) {
+      this.fields.push({
+        type: 'button',
+        templateOptions: {
+          text: `${value}`,
+          id: `${key}`,
+          onClick: ($event) => {
+            this.calcForm.get('display').setValue(`${value}`);
+          },
+        }
+      })
+    }*/
+  }
+
+  submit() {
+   console.log(this.fields);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyNgZorroAntdModule } from '@ngx-formly/ng-zorro-antd';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyNgZorroAntdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
